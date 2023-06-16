e5fd50687552243f531437c43d1d4c17ec13351e
DiMaggi0 <107669577+DiMaggi0@users.noreply.github.com>
2023-06-16 07:51:49 +0300

    Updating Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Hik");
             System.out.println("Введите выражение: ");
             java.util.Scanner enter = new java.util.Scanner(System.in);
             String s = enter.nextLine();
